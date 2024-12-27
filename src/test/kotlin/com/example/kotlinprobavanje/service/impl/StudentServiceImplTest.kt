30c1380b584f7d3f39fa3d45ab11fbc7f3467097
mmihailovic <marko.mihailovic67@yahoo.com>
2024-12-27 21:25:38 +0100

    test fix

--- src/test/kotlin/com/example/kotlinprobavanje/service/impl/StudentServiceImplTest.kt
+++ src/test/kotlin/com/example/kotlinprobavanje/service/impl/StudentServiceImplTest.kt
@@ -1,23 +1,14 @@
 package com.example.kotlinprobavanje.service.impl
 
-import com.example.kotlinprobavanje.repository.StudentRepository
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.extension.ExtendWith
-import org.mockito.InjectMocks
-import org.mockito.Mock
 import org.mockito.junit.jupiter.MockitoExtension
 import java.util.*
 
 @ExtendWith(MockitoExtension::class)
 class StudentServiceImplTest {
-    @InjectMocks
-    private lateinit var studentServiceImpl: StudentServiceImpl
-
-    companion object {
-        const val STUDENT_ID: Long = 15;
-    }
 
     @Test
     fun updateStudent() {
